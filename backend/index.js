e69d94ee33ffb3862586bd404465fe815b8b217f
unknown <abhinasingh2001@gmail.com>
2024-03-05 12:33:19 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try {
+    const response = await axios.put(
+      'https://api.chatengine.io/users/',
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "b51ad6b4-a168-410b-a57e-d18351dba389" } }
+    );
+    return res.status(response.status).json(response.data);
+  } catch (error) {
+    console.error("Error:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
